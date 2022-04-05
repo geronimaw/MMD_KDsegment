ddb2cb1fd242c47b50da302489453e8a0d1970c4
Alessandro Cacciatore <38426513+geronimaw@users.noreply.github.com>
2022-04-05 04:51:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from mmd_tf import MMD as MMD_tf
 if __name__ == '__main__':
     a = torch.tensor([[1., 2.], [2, 3]])
     b = torch.tensor([[4., 6.], [8, 0]])
-    # c = torch.tensor([[1., 1.], [2, 3]])
+    c = torch.tensor([[1., 1.], [2, 3]])
 
     ab = MMD_torch(a, b, "multiscale")
-    # ac = MMD_torch(a, c, "multiscale")
-    # aa = MMD_torch(a, a, "multiscale")
+    ac = MMD_torch(a, c, "multiscale")
+    aa = MMD_torch(a, a, "multiscale")
 
     print("MMD_torch(a,b) = ", ab)
-    # print("MMD_torch(a,c) = ", ac)
-    # print("MMD_torch(a,a) = ", aa)
+    print("MMD_torch(a,c) = ", ac)
+    print("MMD_torch(a,a) = ", aa)
 
     a = tf.constant([[1., 2.], [2, 3]], dtype=tf.float32)
     b = tf.constant([[4., 6.], [8., 0.]], dtype=tf.float32)
-    # c = tf.constant([[1., 1.], [2, 3]])
+    c = tf.constant([[1., 1.], [2, 3]])
 
-    # ab = MMD_tf(a, b, "multiscale")
-    # ac = MMD_tf(a, c, "multiscale")
-    # aa = MMD_tf(a, a, "multiscale")
+    ab = MMD_tf(a, b, "multiscale")
+    ac = MMD_tf(a, c, "multiscale")
+    aa = MMD_tf(a, a, "multiscale")
 
     print("MMD_tf(a,b) = ", aa)
-    # print("MMD_tf(a,c) = ", ac)
-    # print("MMD_tf(a,a) = ", aa)
+    print("MMD_tf(a,c) = ", ac)
+    print("MMD_tf(a,a) = ", aa)
